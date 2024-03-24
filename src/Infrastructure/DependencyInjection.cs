9db971c1605b518554d039ea02b1204e934a098e
Fry <renyuan.fei.1020@gmail.com>
2024-03-24 14:57:47 +1030

    Expose new port and improve deployment setup in Docker and GitHub Actions
    Exposed 8080 port in Dockerfile for new service requirements. Updated GitHub Actions workflow to include a deployment step after docker build, using Fly.io. In addition, the way of handling database connections was updated in DependencyInjection.cs, now supporting connections to different databases based on the runtime environment.

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -90,24 +90,53 @@ public static class DependencyInjection
     }
     else
     {
-      var eventsDbConnection = configuration.GetConnectionString("EventsConnection");
+      string eventsDbConnection;
 
-      GuardValidation.AgainstNull(eventsDbConnection,
-                                  message:
-                                  "Connection string 'EventsConnection' not found.");
+      string identityDbConnection;
 
-      var identityDbConnection = configuration.GetConnectionString("IdentityConnection");
+      var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+      // Depending on if in development or production, use either FlyIO
+      // connection string, or development connection string from env var.
+      if (env == "Development")
+      {
+        eventsDbConnection = configuration.GetConnectionString("EventsConnection");
 
-      GuardValidation.AgainstNull(identityDbConnection,
-                                  message:
-                                  "Connection string 'IdentityConnection' not found.");
+        GuardValidation.AgainstNull(eventsDbConnection, message: "Connection string 'EventsConnection' not found.");
+
+        identityDbConnection = configuration.GetConnectionString("IdentityConnection");
+
+        GuardValidation.AgainstNull(identityDbConnection, message: "Connection string 'IdentityConnection' not found.");
+      }
+      else
+      {
+        // Use connection string provided at runtime by Flyio.
+        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        GuardValidation.AgainstNull(connUrl, message: "Connection string 'connUrl' not found.");
+
+        // Parse connection URL to connection string for Npgsql
+        connUrl = connUrl!.Replace("postgres://", string.Empty);
+        var pgUserPass = connUrl.Split("@")[0];
+        var pgHostPortDb = connUrl.Split("@")[1];
+        var pgHostPort = pgHostPortDb.Split("/")[0];
+        // var pgDb = pgHostPortDb.Split("/")[1];
+        var pgUser = pgUserPass.Split(":")[0];
+        var pgPass = pgUserPass.Split(":")[1];
+        var pgHost = pgHostPort.Split(":")[0];
+        var pgPort = pgHostPort.Split(":")[1];
+
+        const string eventsDb = "Events";
+        const string identityDb = "Identity";
+
+        identityDbConnection = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={eventsDb};";
+        eventsDbConnection = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={identityDb};";
+      }
 
       services.AddDbContext<EventsDbContext>((sp, options) =>
       {
         options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
 
-        options.UseNpgsql(eventsDbConnection, b => b.MigrationsAssembly(typeof
-            (EventsDbContext).Assembly.FullName));
+        options.UseNpgsql(eventsDbConnection, b => b.MigrationsAssembly(typeof(EventsDbContext).Assembly.FullName));
       });
 
       services.AddDbContext<AppIdentityDbContext>(options =>
